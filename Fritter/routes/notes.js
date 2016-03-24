8f2ff76c1153b1e8f17b0f4726772414cd4f1174
Aradhana Sinha <aradhana.sinha@gmail.com>
2016-03-24 18:02:14 -0400

    use moment

--- Fritter/routes/notes.js
+++ Fritter/routes/notes.js
@@ -1,53 +1,10 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 var utils = require('../utils/utils');
 
-var User = require('../models/User')
+var Note = require('../models/Note');
 
-/*
-  Require authentication on ALL access to /notes/*
-  Clients which are not logged in will receive a 403 error code.
-*/
-var requireAuthentication = function(req, res, next) {
-  console.log("routes/notes.js >> Middleware: requireAuthentication")
-  if (!req.currentUser) {
-    utils.sendErrResponse(res, 403, 'Must be logged in to use this feature.');
-  } else {
-    next();
-  }
-};
-
-/*
-  Require ownership whenever accessing a particular note
-  This means that the client accessing the resource must be logged in
-  as the user that originally created the note. Clients who are not owners 
-  of this particular resource will receive a 404.
-
-  Why 404? We don't want to distinguish between notes that don't exist at all
-  and notes that exist but don't belong to the client. This way a malicious client
-  that is brute-forcing urls should not gain any information.
-*/
-var requireOwnership = function(req, res, next) {
-  console.log("routes/notes.js >> Middleware: requireOwnership")
-  if (!(req.currentUser.username === req.note.creator)) {
-    utils.sendErrResponse(res, 404, 'Resource not found.');
-  } else {
-    next();
-  }
-};
-
-/*
-  For create and edit requests, require that the request body
-  contains a 'content' field. Send error code 400 if not.
-*/
-var requireContent = function(req, res, next) {
-  console.log("routes/notes.js >> Middleware: requireContent")
-  if (!req.body.content) {
-    utils.sendErrResponse(res, 400, 'Content required in request.');
-  } else {
-    next();
-  }
-};
 
 /*
   Grab a note from the store whenever one is referenced with an ID in the
@@ -65,10 +22,6 @@ router.param('note', function(req, res, next, noteId) {
   });
 });
 
-// Register the middleware handlers above.
-router.all('*', requireAuthentication);
-router.all('/:note', requireOwnership);
-router.post('*', requireContent);
 
 /*
   At this point, all requests are authenticated and checked:
